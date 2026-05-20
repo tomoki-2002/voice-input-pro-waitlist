498ac55508afb88da1337e989df651d28f10895a
tomoki-2002 <tomoki.nagao@icloud.com>
2026-05-20 14:55:37 +0900

    Set Google Form waitlist URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.VOICE_INPUT_PRO_WAITLIST = {
-  GOOGLE_FORM_URL: "",
+  GOOGLE_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSf6ffXCbIOmRC0TBZmI38TdcMUyvOOIo0a7jh3OF-g-D2HItg/viewform?usp=publish-editor",
   CONTACT_LABEL: ""
 };
